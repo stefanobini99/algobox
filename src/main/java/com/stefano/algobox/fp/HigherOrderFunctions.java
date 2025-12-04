e66edfce0951dd651c3eca3156aeab272a90f62c
stefanobini99 <stefanobini99@gmail.com>
2025-12-04 20:25:01 +0100

    Update HigherOrderFunctions.java

--- src/main/java/com/stefano/algobox/fp/HigherOrderFunctions.java
+++ src/main/java/com/stefano/algobox/fp/HigherOrderFunctions.java
@@ -2,9 +2,19 @@ package com.stefano.algobox.fp;
 
 import java.util.function.Function;
 
+/**
+ * Higher-order functions for functional programming examples.
+ */
 public class HigherOrderFunctions {
 
-    // Accepts a transformation and applies it twice
+    /**
+     * Applies a function twice to a value.
+     *
+     * @param function function to apply
+     * @param value    initial value
+     * @param <T>      type of value
+     * @return result of applying function twice
+     */
     public static <T> T applyTwice(Function<T, T> function, T value) {
         return function.apply(function.apply(value));
     }
